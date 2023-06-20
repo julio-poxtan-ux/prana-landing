cba66b072129dff652407b62dbbc100629f31de5
jucepoxtan-ux <88602319+julio-poxtan-ux@users.noreply.github.com>
2023-06-19 18:27:47 -0600

    correo
    add

---
+++ enviar_correo.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+
+    // Validar y procesar los datos antes de enviar el correo
+
+    $destinatario = 'tu_correo_electronico@example.com';
+    $asunto = 'Solicitud de descarga de PDF';
+    $mensaje = "Nombre: $nombre\n";
+    $mensaje .= "Correo electrónico: $correo\n";
+
+    // Enviar el correo
+    if (mail($destinatario, $asunto, $mensaje)) {
+        http_response_code(200); // Éxito
+    } else {
+        http_response_code(500); // Error en el servidor
+    }
+} else {
+    http_response_code(400); // Solicitud incorrecta
+}
+?>
